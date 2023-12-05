722470aacde260106bf18372a9d23e4a3f6bfbda
Cyril PIN-FERRIER <cyril.pinferrier@outlook.fr>
2023-12-05 18:38:19 +0100

    image principal ok

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ChangelogURL", "https://github.com/CPINFERRIER/Toshoot/commits/master")]
 
 //[Optional] The url to a featured logo that will be displayed in the plugin list next to the name
-[assembly: AssemblyMetadata("FeaturedImageURL", "")]
+[assembly: AssemblyMetadata("FeaturedImageURL", "https://raw.githubusercontent.com/CPINFERRIER/Toshoot/master/toshoot.jpg")]
 //[Optional] A url to an example screenshot of your plugin in action
 [assembly: AssemblyMetadata("ScreenshotURL", "")]
 //[Optional] An additional url to an example example screenshot of your plugin in action
